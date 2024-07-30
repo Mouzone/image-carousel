7b910e259db61eb4620cebe7272135305db1aaae
Mouzone <sunnyliu010@gmail.com>
2024-07-30 12:24:23 -0400

    Refactor code and change global curr_position to relative based on ".active". Add timer and change buttons to arrows to advance or go back

--- script.js
+++ script.js
@@ -1,63 +1,87 @@
-const slideshow = document.getElementById("slideshow")
-const image_element = document.querySelector("img")
-image_links = [
+function makeArrowsInteractive() {
+    const arrows = document.querySelectorAll(".arrow")
+    arrows.forEach(arrow => {
+        arrow.addEventListener("click", event => {
+            const curr_image = document.querySelector(".active")
+            let curr_position = parseInt(curr_image.id)
+            if (event.currentTarget.id === "left") {
+                curr_position -= 1
+                if (curr_position < 0) {
+                    curr_position = IMAGE_LINKS.length - 1
+                }
+            } else {
+                curr_position += 1
+                if (curr_position >= IMAGE_LINKS.length) {
+                    curr_position = 0
+                }
+            }
+
+            changeSlide(curr_position)
+        })
+    })
+}
+
+
+function generateProgressCircles() {
+    const progress_circles = document.getElementById("progress")
+    let counter = 0
+
+    for (let i = 0; i < IMAGE_LINKS.length; i++) {
+        const circle = document.createElement("span")
+        progress_circles.appendChild(circle)
+        circle.id = `${counter}`
+        circle.classList.add("dot")
+        if (counter === 0){
+            circle.classList.add("active")
+        }
+        circle.addEventListener("click", event => {
+            changeSlide(parseInt(event.currentTarget.id))
+        })
+        counter++
+    }
+}
+
+
+function changeSlide(curr_position) {
+    const curr_active = document.querySelector(".active")
+    curr_active.classList.remove("active")
+    const to_be_active = document.getElementById(`${curr_position}`)
+    to_be_active.classList.add("active")
+
+    const image_element = document.querySelector("img")
+    image_element.src = `images/${IMAGE_LINKS[curr_position]}`
+}
+
+
+window.onload = function() {
+    let timeout;
+    function resetTimer() {
+        clearTimeout(timeout);
+        timeout = setTimeout(advanceSlide, 2000); // 5 seconds of inactivity
+    }
+
+    function advanceSlide(element, eventType) {
+        const right_arrow = document.querySelector("#right")
+        const click_event = new Event("click");
+        right_arrow.dispatchEvent(click_event)
+    }
+
+    const arrows = document.querySelectorAll(".arrow")
+    arrows.forEach(arrow => {
+        arrow.addEventListener('click', resetTimer);
+    })
+
+    // Start the timer initially
+    makeArrowsInteractive()
+    generateProgressCircles()
+    resetTimer();
+}
+
+
+const IMAGE_LINKS = [
     "dog-puppy-on-garden-royalty-free-image-1586966191.avif",
     "download.jpeg",
     "download (1).jpeg",
     "download (2).jpeg",
     "images.jpeg"
 ]
-
-let curr_position = 0
-const left_button = document.querySelector("button#left")
-left_button.addEventListener("click", event => {
-    curr_position -= 1
-    if (curr_position < 0) {
-        curr_position = image_links.length - 1
-    }
-    image_element.src = `images/${image_links[curr_position]}`
-
-    const curr_active = document.querySelector(".active")
-    curr_active.classList.remove("active")
-    const to_be_active = document.getElementById(`${curr_position}`)
-    to_be_active.classList.add("active")
-})
-
-const right_button = document.querySelector("button#right")
-right_button.addEventListener("click", event => {
-    curr_position += 1
-    if (curr_position >= image_links.length) {
-        curr_position = 0
-    }
-    image_element.src = `images/${image_links[curr_position]}`
-
-    const curr_active = document.querySelector(".active")
-    curr_active.classList.remove("active")
-    const to_be_active = document.getElementById(`${curr_position}`)
-    to_be_active.classList.add("active")
-})
-
-const progress_circles = document.getElementById("progress")
-let counter = 0
-image_links.forEach(link => {
-    const circle = document.createElement("span")
-    progress_circles.appendChild(circle)
-    circle.id = `${counter}`
-    circle.classList.add("dot")
-    if (counter === 0){
-        circle.classList.add("active")
-    }
-    circle.addEventListener("click", event => {
-        const curr_active = document.querySelector(".active")
-        curr_active.classList.remove("active")
-        const to_be_active = event.currentTarget
-        to_be_active.classList.add("active")
-        image_element.src = `images/${image_links[parseInt(event.currentTarget.id)]}`
-        curr_position = parseInt(event.currentTarget.id)
-    })
-    counter++
-})
-
-
-
-// todo: add auto play based on timer
